8600382e7425f387b97de5285eb1477f78265e4d
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-03-28 09:35:03 +0200

    initial commit

--- module-4/my-typescript/src/counter.ts
+++ module-4/my-typescript/src/counter.ts
@@ -1,14 +1,42 @@
 
 
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
- 
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
 
+
+  //primitive type
+
+  
+  let age: number = 0;
+  let firstName: string = "khulyso";
+
+  //Array type
+
+  let numbers : number[] = [1,2,3,4];
+  let names : string[] = ["khuliso","john","thavhiwa","mapeni"];
+  let mixed : (string | number)[] = ["vhangeno", 23];
+  
+  //objects
+  type User = {
+    id: number;
+    email: string;
+    isAdmin?: boolean;
+  }
+
+  const User: User = {
+    id:24,
+    email: "khulysojohn@gmail.com",
+    isAdmin: true
+  }
+
+
+  console.log(age);
+  console.log(names);
+  console.log(firstName);
+  console.log(mixed);
+  console.log(User);
+  console.log(numbers);
+
+
+const menu = () => {{age}{names}}
+
+menu()
 
