1b5eb590bcaa9d61b70e1b0ba51d8305c681df14
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-04-07 15:54:23 +0200

    initial commit

--- typescript-componets/src/App.tsx
+++ typescript-componets/src/App.tsx
@@ -1,15 +1,14 @@
-import Button from './Components/ButtonComponent'
-import './App.css'
-import withLoading from './Components/withLpading'
+import './App.css';
+import Button from './Components/ButtonComponent';
+import withLoading from './Components/withLoading';
 
 function App() {
-const ButtonWithLoading = withLoading(Button);
-
+  const ButtonWithLoading = withLoading(Button);
   return (
     <>
-     < ButtonWithLoading label="my simple Button" isLoading={true}/>
+      <ButtonWithLoading label="My Simple Button" isLoading={true} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
